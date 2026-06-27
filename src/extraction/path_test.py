9461ee0fce18f4476f97df75e8b4ed9777be1940
Lincoln Wokoma <wokomalincoln@gmail.com>
2026-06-27 22:43:47 +0100

    Removed a test file

--- src/extraction/path_test.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-parent_dir = Path(__file__).parent.parent.parent
-
-file_dir = parent_dir / "data" / "The-Psychology-of-Money.pdf"
-print(f"Parent directory: {parent_dir}")
-print(f"File directory: {file_dir}")    
