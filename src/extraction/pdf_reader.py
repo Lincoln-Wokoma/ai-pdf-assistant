cdb5b5c4126f67016322708588d812e499d1c014
Lincoln Wokoma <wokomalincoln@gmail.com>
2026-06-27 22:41:45 +0100

    Modified My Pdf_Reader to return text and accept file path as input

--- src/extraction/pdf_reader.py
+++ src/extraction/pdf_reader.py
@@ -2,23 +2,22 @@ import pymupdf
 from pathlib import Path
 from typing import Union
 
-def extract_text_from_pdf(pdf_path: Union[str, Path], output_path: Union[str, Path]) -> None:
+def extract_text_from_pdf(pdf_path: Union[str, Path]) -> str:
     """
-    Extracts text from a PDF file and saves it to a text file.
+    Extracts text from a PDF file.
 
     Args:
         pdf_path (Union[str, Path]): The path to the PDF file.
-        output_path (Union[str, Path]): The path to the output text file.
     """
     doc = pymupdf.open(pdf_path)
+    text = ""
 
-    with open(output_path, "wb") as out:
-        for page in doc:
-            text = page.get_text("text")
-            out.write(text.encode("utf-8"))
-            #out.write(bytes((12,)))  # Form feed character to separate pages
+    for page in doc:
+        text += page.get_text("text")
 
     doc.close()
+    return text
 
 if __name__ == "__main__":
-    print(extract_text_from_pdf(Path("C:/Users/Lincoln/Documents/Projects/AI_Automation & Engineering/ai-pdf-assistant/data/The-Psychology-of-Money.pdf"), Path("C:/Users/Lincoln/Documents/Projects/AI_Automation & Engineering/ai-pdf-assistant/data/processed_data/The-Psychology-of-Money.txt")))
+    doc_path = input("Enter the path to the PDF file: ")
+    print(extract_text_from_pdf(Path(doc_path)))
